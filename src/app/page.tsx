642a86b0fafdfd1619fbd8498060c4cefb57c8ef
ekpenyong-bassey <ephraimbassey58@gmail.com>
2025-10-29 13:44:05 +0100

    chore: update dependencies and improve layout
    - Updated `next` to version 16.0.1 and `tailwindcss` to version 4.1.16 in package.json and package-lock.json.
    - Added `autoprefixer` and `lightningcss` as dependencies.
    - Updated `postcss` to version 8.5.6.
    - Removed unused font imports and adjusted the layout in `layout.tsx`.
    - Modified the main page in `page.tsx` to include a title and a sun icon.
    - Cleaned up `tsconfig.json` for better readability and organization.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import { Moon, Sun } from "lucide-react";
+import { SunIcon } from "lucide-react";
 import Image from "next/image";
+import Todo from "@/Components/Todo";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Moon className="text-sky-400" />
-      </main>
-    </div>
+
+    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <h1>TODO</h1>
+        <SunIcon />
+      </div>
+    </main>
+
   );
 }
